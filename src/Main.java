4ff9012d23a52951fec2a34e194cb79b9c442206
Ritik <hello@y.com>
2018-11-03 01:22:10 +0530

    Change in main file

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,26 @@
-package PACKAGE_NAME;
+import javax.swing.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
 
 public class Main {
+
+
+    public static void main(String[] args) {
+        System.out.println("ahello");
+        Main m = new Main();
+
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+//                LandingPage l= new LandingPage();
+
+            }
+        });
+
+    }
+
 }
